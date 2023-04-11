faba0491b7bab076d4d13d591d8f904fbeb1e8c5
Mohamed <Mohamed24taa@gmail.com>
2023-04-11 05:54:06 +0200

    make it better

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:app
+echo "web: python main.py"
---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request
+import pickle
+import numpy as np
+model = pickle.load(open('RandomForest-model.pkl', 'rb'))
+app= Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello world "
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+        Age = request.form.get('Age')
+        Gender = request.form.get('Gender')
+        Total_Bilirubin = request.form.get('Total_Bilirubin')
+        Direct_Bilirubin = request.form.get('Direct_Bilirubin')
+        Alkaline_Phosphotase = request.form.get('Alkaline_Phosphotase')
+        Alamine_Aminotransferase = request.form.get('Alamine_Aminotransferase')
+        Aspartate_Aminotransferase = request.form.get('Aspartate_Aminotransferase')
+        Total_Protiens = request.form.get('Total_Protiens')
+        Albumin = request.form.get('Albumin')
+        Albumin_and_Globulin_Ratio = request.form.get('Albumin_and_Globulin_Ratio')
+
+        input_query = np.array([[Age, Gender, Total_Bilirubin, Direct_Bilirubin, Alkaline_Phosphotase, Alamine_Aminotransferase,
+                      Aspartate_Aminotransferase, Total_Protiens, Albumin, Albumin_and_Globulin_Ratio]])
+        result = model.predict(input_query)[0]
+
+        return jsonify({'placement ': str(result)})
+
+
+if __name__ == '__main__':
+    app.run()
+
+
