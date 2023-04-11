5c2861eb1ee6bc849892ca9cfd1793ed3a7faa05
Mohamed <Mohamed24taa@gmail.com>
2023-04-11 04:15:47 +0200

    make it better

--- app.py
+++ app.py
@@ -1,35 +1,50 @@
 from flask import Flask, jsonify, request
 import pickle
 import numpy as np
-model = pickle.load(open('RandomForest-model.pkl', 'rb'))
+# model = pickle.load(open('RandomForest-model.pkl', 'rb'))
+
+
 
 app = Flask(__name__)
-
-
 @app.route('/')
 def home():
-    return "Hello World!"
+
+        return "Hello World!"
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#         Age = request.form.get('Age')
+#         Gender = request.form.get('Gender')
+#         Total_Bilirubin = request.form.get('Total_Bilirubin')
+#         Direct_Bilirubin = request.form.get('Direct_Bilirubin')
+#         Alkaline_Phosphotase = request.form.get('Alkaline_Phosphotase')
+#         Alamine_Aminotransferase = request.form.get('Alamine_Aminotransferase')
+#         Aspartate_Aminotransferase = request.form.get('Aspartate_Aminotransferase')
+#         Total_Protiens = request.form.get('Total_Protiens')
+#         Albumin = request.form.get('Albumin')
+#         Albumin_and_Globulin_Ratio = request.form.get('Albumin_and_Globulin_Ratio')
+#
+#         input_query = np.array([[Age, Gender, Total_Bilirubin, Direct_Bilirubin, Alkaline_Phosphotase, Alamine_Aminotransferase,
+#                       Aspartate_Aminotransferase, Total_Protiens, Albumin, Albumin_and_Globulin_Ratio]])
+#         result = model.predict(input_query)[0]
+#
+#         return jsonify({'placement ': str(result)})
 
 
-@app.route('/predict', methods=['POST'])
-def predict():
-        Age = request.form.get('Age')
-        Gender = request.form.get('Gender')
-        Total_Bilirubin = request.form.get('Total_Bilirubin')
-        Direct_Bilirubin = request.form.get('Direct_Bilirubin')
-        Alkaline_Phosphotase = request.form.get('Alkaline_Phosphotase')
-        Alamine_Aminotransferase = request.form.get('Alamine_Aminotransferase')
-        Aspartate_Aminotransferase = request.form.get('Aspartate_Aminotransferase')
-        Total_Protiens = request.form.get('Total_Protiens')
-        Albumin = request.form.get('Albumin')
-        Albumin_and_Globulin_Ratio = request.form.get('Albumin_and_Globulin_Ratio')
-
-        input_query = np.array([[Age, Gender, Total_Bilirubin, Direct_Bilirubin, Alkaline_Phosphotase, Alamine_Aminotransferase,
-                      Aspartate_Aminotransferase, Total_Protiens, Albumin, Albumin_and_Globulin_Ratio]])
-        result = model.predict(input_query)[0]
-
-        return jsonify({'placement ': str(result)})
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+# from flask import Flask, jsonify, request, render_template, testing
+# import pickle
+# import numpy as np
+#
+#
+#
+# def create_app(testing: bool =True):
+#         app= Flask(__name__)
+#         @app.route('/')
+#         def index():
+#                 return f"Hello world<br> Testing: {testing}"
+#         return app
+#
+# # if __name__ == '__main__':
+# #     app.run(port=5004)
